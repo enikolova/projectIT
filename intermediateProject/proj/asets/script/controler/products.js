9b1b71b67ed84d8b98a91610dcd03e001f72006d
Elena Nikolova <enikolova@users.noreply.github.com>
2017-10-01 00:32:35 +0300

    added more products to json

--- intermediateProject/proj/asets/script/controler/products.js
+++ intermediateProject/proj/asets/script/controler/products.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded',function(){
     getProducts().then(function(products){
         productsList._productList=Array.prototype.slice.call(products.products,0);
-        console.log(productsList._productList);
-    }).catch();
-
-   var templateText=document.getElementById('product-template').innerHTML;
+        console.log(productsList);
+      var templateText=document.getElementById('product-template').innerHTML;
    var template=Handlebars.compile(templateText);
    var readyTemp=template(productsList);
-   console.log(readyTemp);
    document.getElementById('products').innerHTML=readyTemp;
+    }).catch();
+
+  
 })
