81449bcffe888759417dff7c3765726d6273ef05
Elena Nikolova <enikolova@users.noreply.github.com>
2017-10-01 09:32:20 +0300

    updated json

--- intermediateProject/proj/asets/script/controler/products.js
+++ intermediateProject/proj/asets/script/controler/products.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
     getProducts().then(function(products){
         productsList._productList=Array.prototype.slice.call(products.products,0);
-        console.log(productsList);
+    
       var templateText=document.getElementById('product-template').innerHTML;
    var template=Handlebars.compile(templateText);
    var readyTemp=template(productsList);
