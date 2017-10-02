de6d700ad8797383848a02126973957f813527d8
varadinov90 <31103510+varadinov90@users.noreply.github.com>
2017-10-02 08:45:55 +0300

    early morning fixed
    without username is not nessesary

--- intermediateProject/proj/asets/script/serveses/userService.js
+++ intermediateProject/proj/asets/script/serveses/userService.js
@@ -1,20 +1,19 @@
 var userList = (function () {
-    function User(username, password, email) {
-        this.username = username;
+    function User(email, password) {
         this.password = password;
         this.email = email;
         this.favorites = [];
         this.shopingCart = [];
-        
+
     }
     User.prototype.addToFavorites = function (item) {
         this.favorites.push(item);
-       
+
     }
     User.prototype.removeFromFavorites = function (item) {
         var id = this.favorites.findIndex(el => el == item.name)
         this.favorites.splice(id, 1);
-        
+
 
     }
     User.prototype.addToShopingCard = function (item) {
@@ -33,26 +32,25 @@ var userList = (function () {
             localStorage.setItem('users', JSON.stringify(this._users));
         }
     }
-    UserList.prototype=Object.create(User.prototype);
-    UserList.prototype.constructor=UserList;
+    UserList.prototype = Object.create(User.prototype);
+    UserList.prototype.constructor = UserList;
 
-    UserList.prototype.addToFavorites=function(item){
-        UserList.prototype.addToFavorites.call(this,item);
+    UserList.prototype.addToFavorites = function (item) {
+        UserList.prototype.addToFavorites.call(this, item);
         localStorage.setItem('users', JSON.stringify(this._users));
     }
-    UserList.prototype.addUser = function (username, password, email) {
-        if ((typeof username == 'string') && (username.trim().length > 3) &&
-            (password.trim().length > 5) && email) {
-            if (!(this._users.some(user => user.username === username))) {
-                this._users.push(new User(username, password, email));
+    UserList.prototype.addUser = function (email, password) {
+        if ((password.trim().length > 5) && email) {
+            if (!(this._users.some(user => user.email === email))) {
+                this._users.push(new User(password, email));
                 localStorage.setItem('users', JSON.stringify(this._users));
             }
         }
     }
 
 
-    UserList.prototype.login = function (username, password) {
-        return this._users.some(user => user.username === username &&
+    UserList.prototype.login = function (email,password) {
+        return this._users.some(user => user.email === email &&
             user.password === password);
     }
 
