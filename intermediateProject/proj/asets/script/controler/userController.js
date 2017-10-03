a7f7212ef5496d76cc3b042d680fc8b4a59cd2b8
varadinov90 <31103510+varadinov90@users.noreply.github.com>
2017-10-03 12:18:19 +0300

    update user and login
    update user and login and more

--- intermediateProject/proj/asets/script/controler/userController.js
+++ intermediateProject/proj/asets/script/controler/userController.js
@@ -1,15 +1,33 @@
-$(function () {
-    $("#reg_con").click(function () {
-         event.preventDefault();
-        var email =document.querySelectorAll("input.form-control")[0].value;
-        if ($("#reg input.form-control")[1].val == $("#reg input.form-control")[2].value) {
-            var password = $("input.form-control")[1];
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('reg_con').addEventListener('click', function (event) {
+        event.preventDefault();
+        var email = document.querySelectorAll("#reg input.form-control")[0].value;
+        if ($("#reg input.form-control")[1].value != "" && $("#reg input.form-control")[2].value != "" && $("#reg input.form-control")[1].value == $("#reg input.form-control")[2].value) {
+            var password = $("#reg input.form-control")[1].value;
             userList.addUser(email, password);
             $("#reg").hide();
             $("#login").show();
         } else {
-            $("input.form-control")[1] = "";
-            $("input.form-control")[2] = "";
+            $("#reg input.form-control")[1].value = "";
+            $("#reg input.form-control")[2].value = "";
         }
     })
+
+
+    document.querySelector('#login button').addEventListener('click', function (event) {
+        event.preventDefault()
+        var email = document.querySelectorAll("#login input.form-control")[0].value;
+        var password = document.querySelectorAll("#login input.form-control")[1].value;
+        $("#main,.footer").css("display","block");
+        location.hash="#home";
+        $("#mainPage").hide();
+        setTimeout(function() {
+            location.hash="";
+        }, 0);
+    })
+window.onunload=function(){
+     location.hash="";
+
+    location.href=location.href.slice(0,49)
+}
 })
--- intermediateProject/proj/asets/script/serveses/userService.js
+++ intermediateProject/proj/asets/script/serveses/userService.js
@@ -1,28 +1,32 @@
 var userList = (function () {
+    var id = 1
     function User(email, password) {
         this.password = password;
         this.email = email;
         this.favorites = [];
         this.shopingCart = [];
-
+        this.id = id++;
+        this.isLoged = false;
     }
+
     User.prototype.addToFavorites = function (item) {
         this.favorites.push(item);
-       localStorage.setItem('users', JSON.stringify(userList._users));
+        localStorage.setItem('users', JSON.stringify(userList._users));
 
     }
     User.prototype.removeFromFavorites = function (item) {
         var id = this.favorites.findIndex(el => el == item.name)
         this.favorites.splice(id, 1);
-
-
+        localStorage.setItem('users', JSON.stringify(userList._users));
     }
     User.prototype.addToShopingCard = function (item) {
         this.shopingCart.push(item);
+        localStorage.setItem('users', JSON.stringify(userList._users));
     }
     User.prototype.removeFromShopingCart = function (item) {
         var id = this.shopingCart.findIndex(el => el == item.name)
         this.shopingCart.splice(id, 1);
+        localStorage.setItem('users', JSON.stringify(userList._users));
     }
 
     function UserList() {
@@ -37,7 +41,7 @@ var userList = (function () {
     UserList.prototype.addUser = function (email, password) {
         if (email) {
             if (!(this._users.some(user => user.email === email))) {
-                this._users.push(new User(password, email));
+                this._users.push(new User(email, password));
                 localStorage.setItem('users', JSON.stringify(this._users));
             }
         }
@@ -45,8 +49,21 @@ var userList = (function () {
 
 
     UserList.prototype.login = function (email, password) {
-        return this._users.some(user => user.email === email &&
-            user.password === password);
+        var user = this._users.find(user => user.email === email &&
+            user.password === password)
+        if (user) {
+            user.isLoged = true;
+            return user;
+        } else {
+            return null;
+        }
+    }
+    UserList.prototype.logout=function(id){
+        var user=this._users.find(user => user.id==id && user.isLoged==true);
+        if(user){
+            user.isLoged=false;
+            return true;
+        }return false;
     }
 
     return new UserList();
